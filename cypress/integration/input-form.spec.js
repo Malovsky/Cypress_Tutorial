c75afb532b6a2a710e52fafe6ba40864730585df
Kevin Maslowski <kevinmaslowski@gmail.com>
2022-07-11 11:04:02 +0200

    tuto 3, tested api

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -7,10 +7,41 @@ describe("Input form", () => {
     cy.focused().should("have.class", "new-todo");
   });
 
-  it.only("accepts input", () => {
+  it("accepts input", () => {
     const typedText = "Buy Milk";
     cy.get(".new-todo").type(typedText).should("have.value", typedText);
   });
-});
 
-// LANCER LA VIDEO 3
+  context("Form submission", () => {
+    beforeEach(() => {
+      cy.server();
+    });
+    it("Adds a new todo on submit", () => {
+      const itemText = "Buy eggs";
+      cy.route("POST", "/api/todos", {
+        name: itemText,
+        id: 1,
+        isComplete: false,
+      });
+      cy.get(".new-todo")
+        .type(itemText)
+        .type("{enter}")
+        .should("have.value", "");
+      cy.get(".todo-list li").should("have.length", 1).and("contain", itemText);
+    });
+
+    it("Shows an error message on a failed submission", () => {
+      cy.route({
+        url: "/api/todos",
+        method: "POST",
+        status: 500,
+        response: {},
+      });
+      cy.get(".new-todo").type("test{enter}");
+
+      cy.get(".todo-list li").should("not.exist");
+
+      cy.get(".error").should("be.visible");
+    });
+  });
+});
--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 import Footer from "./Footer";
+import { saveTodo } from "../lib/service";
 
 export default class TodoApp extends Component {
   constructor(props) {
@@ -13,10 +14,24 @@ export default class TodoApp extends Component {
       todos: [],
     };
     this.handleNewTodoChange = this.handleNewTodoChange.bind(this);
+    this.handleTodoSubmit = this.handleTodoSubmit.bind(this);
   }
 
   handleNewTodoChange(e) {
     this.setState({ currentTodo: e.target.value });
+  }
+
+  handleTodoSubmit(e) {
+    e.preventDefault();
+    const newTodo = { name: this.state.currentTodo, isComplete: false };
+    saveTodo(newTodo)
+      .then(({ data }) =>
+        this.setState({
+          todos: this.state.todos.concat(data),
+          currentTodo: "",
+        })
+      )
+      .catch(() => this.setState({ error: true }));
   }
 
   render() {
@@ -25,9 +40,11 @@ export default class TodoApp extends Component {
         <div>
           <header className="header">
             <h1>todos</h1>
+            {this.state.error && <span className="error">Oh no!</span>}
             <TodoForm
               currentTodo={this.state.currentTodo}
               handleNewTodoChange={this.handleNewTodoChange}
+              handleTodoSubmit={this.handleTodoSubmit}
             />
           </header>
           <section className="main">
