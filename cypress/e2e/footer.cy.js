a664244ffc7bb625ad5a66d500cf0151ab06e0df
Kevin Maslowski <kevinmaslowski@gmail.com>
2022-07-12 15:12:58 +0200

    added filters and tested it

--- cypress/e2e/footer.cy.js
+++ cypress/e2e/footer.cy.js
@@ -7,11 +7,27 @@ describe("Footer", () => {
   });
 
   context("with multiple todos", () => {
-    it.only("displays plural todos in count", () => {
+    beforeEach(() => {
       // Sans argument 'seedAndVisit()' aura les arguments par défaut
       // 'seedAndVisit' est dans /cypress/support/commands.js et prend todos.json en param par défaut
       cy.seedAndVisit();
+    });
+
+    it("displays plural todos in count", () => {
       cy.get(".todo-count").should("contain", "3 todos left");
+    });
+
+    it.only("Handles filter links", () => {
+      const filters = [
+        { link: "Active", expectedLength: 3 },
+        { link: "Completed", expectedLength: 1 },
+        { link: "All", expectedLength: 4 },
+      ];
+
+      cy.wrap(filters).each((filter) => {
+        cy.contains(filter.link).click();
+        cy.get(".todo-list li").should("have.length", filter.expectedLength);
+      });
     });
   });
 });
